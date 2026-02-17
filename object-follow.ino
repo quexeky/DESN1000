a5046160fff50c9ad658adc5336c88d0601adb36
quexeky <git@quexeky.dev>
2026-02-17 23:34:49 +1100

    Add regions for cleaner code & slight refactoring on moveRobot and focusObject to clarify data flow

--- object-follow.ino
+++ object-follow.ino
@@ -1,8 +1,8 @@
+#pragma region Imports
 #include <Pixy2.h>
 #include <L298NX2.h>
-
-Pixy2 pixy;
-
+#pragma endregion
+#pragma region Pins
 const unsigned int EN_A = 3;
 const unsigned int IN1_A = 5;
 const unsigned int IN2_A = 6;
@@ -14,14 +14,27 @@ const unsigned int EN_B = 9;
 // For if we need just plain PWM pins
 // int motor[2][3] = [[EN_A, IN1_A, IN2_A], [IN1_B, IN2_B, EN_B]];
 
+#pragma endregion Pins
+#pragma region Control
 L298NX2 motors(EN_A, IN1_A, IN2_A, EN_B, IN1_B, IN2_B);
+Pixy2 pixy;
+#pragma endregion
+#pragma region Config
+const float DEADZONE = 0.15;
 
-const float deadzone = 0.15;
+const int TURN_HIGH = 150;
+const int TURN_LOW = 20;
 
-const int turnHigh = 150;
-const int turnLow = 20;
-
-int signature, x, width;
+const int SCREEN_W = 320;
+const int SCREEN_H = 240;
+#pragma endregion
+#pragma region Typedefs
+struct BoundingBox {
+  int x;
+  int y;
+  int width;
+  int height;
+};
 
 enum TurnSpeed {
   ForwardHigh,
@@ -29,6 +42,20 @@ enum TurnSpeed {
   BackHigh,
   BackLow
 };
+
+int getTurnSpeedValue(TurnSpeed speed) {
+  switch (speed) {
+    case ForwardHigh:
+      return TURN_HIGH;
+    case ForwardLow:
+      return TURN_LOW;
+    case BackHigh:
+      return TURN_HIGH;
+    case BackLow:
+      return TURN_LOW;
+  }
+}
+#pragma endregion
 
 void setup() {
   Serial.begin(115200);
@@ -43,7 +70,14 @@ void loop() {
   if (pixy.ccc.numBlocks) {
     debugBlocks();
     focusObject();
+    noBlockDetected();
+  } else {
+
   }
+}
+void noBlockDetected() {
+  // Placeholder for things like going back and turning around 
+  // after a certain amount of time has passed
 }
 
 void debugBlocks() {
@@ -57,9 +91,10 @@ void debugBlocks() {
   }
 }
 void focusObject() {
-  float centre = getCentre();
+  BoundingBox box = getPixyCamBoundingBox();
+  float centre = getCentre(box);
 
-  if (-deadzone <= centre && centre <= deadzone) {
+  if (-DEADZONE <= centre && centre <= DEADZONE) {
     centre = 0;
     moveRobot(ForwardLow, ForwardLow);
   } else if (centre < 0) {
@@ -69,52 +104,65 @@ void focusObject() {
   }
 }
 
-float getCentre() {
-  signature = pixy.ccc.blocks[0].m_signature;
-  x = pixy.ccc.blocks[0].m_x;
-  width = pixy.ccc.blocks[0].m_width;
+BoundingBox getPixyCamBoundingBox() {
+  Block block = pixy.ccc.blocks[0];
+  return BoundingBox{
+    block.m_x,
+    block.m_y,
+    block.m_width,
+    block.m_height
+  };
+}
 
+float getCentre(BoundingBox box) {
   // Get the centre of the object to keep that in the middle
-  float midpointX = (x + (width / 2));
+  float midpointX = (box.x + (box.width / 2));
 
-  return map(midpointX, 0, 320, -1, 1);
+  return map(midpointX, 0, SCREEN_W, -1, 1);
 }
 
 void moveRobot(TurnSpeed a, TurnSpeed b) {
   switch (a) {
     case ForwardHigh:
-      motors.setSpeedA(turnHigh);
+      Serial.println("Set A to ForwardHigh");
       motors.forwardA();
       break;
     case ForwardLow:
-      motors.setSpeedA(turnLow);
+      Serial.println("Set A to ForwardLow");
       motors.forwardA();
       break;
     case BackHigh:
-      motors.setSpeedA(turnHigh);
+      Serial.println("Set A to BackHigh");
       motors.backwardA();
       break;
     case BackLow:
-      motors.setSpeedA(turnLow);
+      Serial.println("Set A to BackLow");
       motors.backwardA();
       break;
-  }
+  };
+
   switch (b) {
     case ForwardHigh:
-      motors.setSpeedB(turnHigh);
+      Serial.println("Set B to ForwardHigh");
       motors.forwardB();
       break;
     case ForwardLow:
-      motors.setSpeedB(turnLow);
+      Serial.println("Set B to ForwardLow");
       motors.forwardB();
       break;
     case BackHigh:
-      motors.setSpeedB(turnHigh);
-      motors.backwardA();
+      Serial.println("Set B to BackHigh");
+      motors.backwardB();
       break;
     case BackLow:
-      motors.setSpeedB(turnLow);
-      motors.backwardA();
+      Serial.println("Set B to BackLow");
+      motors.backwardB();
       break;
   }
+  int a_val = getTurnSpeedValue(a);
+  int b_val = getTurnSpeedValue(b);
+  Serial.println("TurnSpeed A: " + a_val);
+  Serial.println("TurnSpeed B: " + b_val);
+  motors.setSpeedA(a_val);
+  motors.setSpeedB(b_val);
 }
